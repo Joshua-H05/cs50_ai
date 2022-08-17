d5ca472553121279e0d4a55d4613b07783da3958
Joshua <joshuayuanhu@gmail.com>
2022-08-18 00:30:06 +0200

    updates

--- natural_language/parser/parser.py
+++ natural_language/parser/parser.py
@@ -1,6 +1,5 @@
 import nltk
 import sys
-import pysnooper
 
 TERMINALS = """
 Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
@@ -58,7 +57,6 @@ def main():
             print(" ".join(np.flatten()))
 
 
-@pysnooper.snoop()
 def reformat(sentence: list) -> list:
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
